92b93cc04f99d91dba64ab005b3d1f30d4405274
Christian Tietze <christian.tietze@googlemail.com>
2012-06-16 18:25:08 +0200

    implemented addition of new list elements in new form

--- handler.js
+++ handler.js
@@ -1,62 +1,50 @@
-// < https://github.com/senchalabs/jQTouch/wiki/Initialization%20Options >
-var jQT = new $.jQTouch({
-    // icon: 'jqtouch.png',
-    // icon4: 'jqtouch4.png',
-    // addGlossToIcon: false,
-    // startupScreen: 'jqt_startup.png',
-    statusBar: 'default',
-    preloadImages: []
-});
 // 
-// // Some sample Javascript functions:
-// $(function(){
+//  handler.js
+//  iCal Templater
+//  
+//  Created by Christian Tietze on 2012-06-16.
 // 
-//     // Show a swipe event on swipe test
-//     $('#swipeme').swipe(function(evt, data) {
-//         var details = !data ? '': '<strong>' + data.direction + '/' + data.deltaX +':' + data.deltaY + '</strong>!';
-//         $(this).html('You swiped ' + details );
-//         $(this).parent().after('<li>swiped!</li>')
-//     });
-// 
-//     $('#tapme').tap(function(){
-//         $(this).parent().after('<li>tapped!</li>')
-//     });
-// 
-//     $('a[target="_blank"]').bind('click', function() {
-//         if (confirm('This link opens in a new window.')) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     });
-// 
-//     // Page animation callback events
-//     $('#pageevents').
-//         bind('pageAnimationStart', function(e, info){ 
-//             $(this).find('.info').append('Started animating ' + info.direction + '&hellip;  And the link ' +
-//                 'had this custom data: ' + $(this).data('referrer').data('custom') + '<br>');
-//         }).
-//         bind('pageAnimationEnd', function(e, info){
-//             $(this).find('.info').append('Finished animating ' + info.direction + '.<br><br>');
-// 
-//         });
-//     
-//     // Page animations end with AJAX callback event, example 1 (load remote HTML only first time)
-//     $('#callback').bind('pageAnimationEnd', function(e, info){
-//         // Make sure the data hasn't already been loaded (we'll set 'loaded' to true a couple lines further down)
-//         if (!$(this).data('loaded')) {
-//             // Append a placeholder in case the remote HTML takes its sweet time making it back
-//             // Then, overwrite the "Loading" placeholder text with the remote HTML
-//             $(this).append($('<div>Loading</div>').load('ajax.html .info', function() {        
-//                 // Set the 'loaded' var to true so we know not to reload
-//                 // the HTML next time the #callback div animation ends
-//                 $(this).parent().data('loaded', true);  
-//             }));
-//         }
-//     });
-//     // Orientation callback event
-//     $('#jqt').bind('turn', function(e, data){
-//         $('#orient').html('Orientation: ' + data.orientation);
-//     });
-//     
-// });
+
+(function() {
+    // global initializer
+    init = function() {
+        // Bind events on items
+        $('.template_overview .template').bind('tap', function(event) {
+            console.log('test');
+        });
+        $('#new_entry .done').tap(function(event) {
+            console.log('add');
+            Templates.add('new');
+        });
+        $('#assign .event_title').text('test!!');
+//        $('#assign .event_time')
+    };
+    
+    var Templates = {
+        selector: $('.template_overview .templates'),
+
+        add: function(title) {
+            if (!title) {
+                throw new StupidError("no title");
+            }
+            
+            var new_elem = '<li>'
+                         + '    <a href="#assign" class="template" data-transition="slideup">'
+                         + '        <h3 class="ui-li-heading">' + title + '</h3>'
+                         + '    </a>'
+                         + '</li>';
+            // Insert element, then refresh list to apply style
+            this.selector.append(new_elem);
+            this.selector.listview("refresh"); 
+            // FIXME cannot call methods on listview prior to initialization; attempted to call method 'refresh'
+        }
+    };
+    
+    // Programmatical errors
+    function StupidError(message) {
+      this.name = "StupidError";
+      this.message = (message || "");
+    }
+    StupidError.prototype = new Error();
+    StupidError.prototype.constructor = StupidError;
+})();
